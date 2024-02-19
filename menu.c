793e4859f613fcdb95010125cf5bc450e01d9013
andrewstephengames <andrewstephengames999@gmail.com>
2024-02-19 23:44:52 +0200

    Add draw_input

--- menu.c
+++ menu.c
@@ -4,10 +4,17 @@ Vector2 rec_to_v (Rectangle rec) {
      return (Vector2) { rec.x, rec.y };
 }
 
+void center_element (Element *e, Vector2 canvas) {
+     float x = canvas.x/2-MeasureText(e->label, e->label_size)/2;
+     float y = canvas.y/2-e->label_size/2;
+     e->box.x = x;
+     e->box.y = y;
+}
+
 void mouse_on_element (Element *e, void (*l_func) (void), void (*r_func) (void)) {
      Vector2 mouse = GetMousePosition();
      if (CheckCollisionPointRec (mouse, e->box)) {
-          e->bg.a = 0x80;
+          e->bg.a = 127;
           if (IsMouseButtonPressed (MOUSE_BUTTON_LEFT) && l_func != NULL) {
                (*l_func)();
           }
@@ -15,15 +22,8 @@ void mouse_on_element (Element *e, void (*l_func) (void), void (*r_func) (void))
                (*r_func)();
           }
      } else {
-          e->bg.a = 0xFF;
+          e->bg.a = 255;
      }
-}
-
-void center_element (Element *e, Vector2 canvas) {
-     float x = canvas.x/2-MeasureText(e->label, e->label_size)/2;
-     float y = canvas.y/2-e->label_size/2;
-     e->box.x = x;
-     e->box.y = y;
 }
 
 void draw_button (Element *e, bool texture, bool center, Vector2 canvas, 
@@ -54,4 +54,47 @@ void draw_label (Element *e, Vector2 canvas, bool texture) {
      DrawText (e->label, e->box.x, e->box.y, e->label_size, e->fg);
 }
 
-//Element *draw_input (Element *e, 
+// FIXME: Characters not getting drawn
+void draw_input (Element *e, Vector2 canvas, bool texture) {
+     bool is_selected = false;
+     center_element (e, canvas);
+     e->box.width = MeasureText(e->label, e->label_size);
+     e->box.height = e->label_size;
+     if (texture) {
+          DrawTextureRec (e->texture, e->box, rec_to_v (e->box), e->bg);
+     } else {
+          DrawRectangleRec (e->box, e->bg);
+     }
+     if (strlen(e->label) == 0) {
+          strcpy (e->label, "Type..");
+     }
+     DrawText (e->label, e->box.x, e->box.y, e->label_size, e->fg);
+     if (CheckCollisionPointRec (GetMousePosition(), e->box)) {
+          SetMouseCursor (MOUSE_CURSOR_IBEAM);
+          e->fg.a = 255;
+          if (IsMouseButtonPressed (MOUSE_BUTTON_LEFT)) {
+               is_selected = true;
+          }
+     } else {
+          SetMouseCursor (MOUSE_CURSOR_DEFAULT);
+          e->fg.a = 50;
+          if (IsMouseButtonPressed (MOUSE_BUTTON_LEFT)) {
+               is_selected = false;
+          }
+     }
+     if (is_selected) {
+          int key = GetCharPressed();
+          if (key >= 32 && key <= 125) {
+               size_t len = strlen(e->label);
+               if (len < sizeof (e->label)-1) {
+                    e->label[len] = (char) key;
+                    e->label[++len] = '\0';
+               }
+          } else if (key == KEY_BACKSPACE) {
+               size_t len = strlen(e->label);
+               if (len > 0) {
+                    e->label[len-1] = '\0';
+               }
+          }
+     }
+}
--- menu.h
+++ menu.h
@@ -2,8 +2,10 @@
 #define MENU_H_
 
 #include <raylib.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 #ifndef __cplusplus
 #include <stdbool.h>
 #endif // __cplusplus
@@ -18,10 +20,11 @@ typedef struct {
 } Element;
 
 Vector2 rec_to_v (Rectangle rec);
-void mouse_on_element (Element *e, void (*l_func) (void), void (*r_func) (void));
 void center_element (Element *e, Vector2 canvas);
+void mouse_on_element (Element *e, void (*l_func) (void), void (*r_func) (void));
 void draw_button (Element *e, bool texture, bool center, Vector2 canvas, 
                   void (*l_func) (void), void (*r_func) (void));
 void draw_label (Element *e, Vector2 canvas, bool texture);
+void draw_input (Element *e, Vector2 canvas, bool texture);
 
 #endif // MENU_H_
